d81c9477af767f1b4953f1c194c9b484f099f997
liuhua520 <165395522+liuhua520-svg@users.noreply.github.com>
2026-06-23 11:56:16 +0800

    Delete qt_desktop directory

--- qt_desktop/__init__.py
+++
@@ -1,8 +0,0 @@
-# locales/__init__.py
-from .en import STRINGS as en
-from .zh_cn import STRINGS as zh_cn
-from .zh_tw import STRINGS as zh_tw
-from .ja import STRINGS as ja
-from .ko import STRINGS as ko
-
-__all__ = ["en", "zh_cn", "zh_tw", "ja", "ko"]
