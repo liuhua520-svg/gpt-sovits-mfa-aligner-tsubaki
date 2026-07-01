5d41a38d68a9868791987cbe106306c6bea91ea2
liuhua520 <165395522+liuhua520-svg@users.noreply.github.com>
2026-07-01 16:41:36 +0800

    Add files via upload

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,10 +3,12 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import { getElementPlusLocale, i18n } from './i18n'
+import { router } from './router'
 
 const app = createApp(App)
 
 app.use(i18n)
+app.use(router)
 app.use(ElementPlus, {
   locale: getElementPlusLocale(i18n.global.locale.value),
 })
